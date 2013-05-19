4fae07526502128921314566f3a62eb9a6f5d12c
Marc Lee <marc@adviso.com.au>
2013-05-19 20:47:20 +1000

    new totoro black background

---
+++ src/totoro.c
@@ -0,0 +1,106 @@
+#include "pebble_os.h"
+#include "pebble_app.h"
+#include "pebble_fonts.h"
+
+// 2B9C2F06-236A-48A0-BEBF-1063B498D244
+#define MY_UUID {0x2B, 0x9C, 0x2F, 0x06, 0x23, 0x6A, 0x48, 0xA0, 0xBE, 0xBF, 0x10, 0x63, 0xB4, 0x98, 0xD2, 0x44}
+
+PBL_APP_INFO(MY_UUID, "Totoro Black", "Marc Lee", 1, 0 /* App version */, RESOURCE_ID_IMAGE_MENU_ICON, APP_INFO_WATCH_FACE);
+
+Window window;
+
+TextLayer text_date_layer;
+TextLayer text_time_layer;
+
+BmpContainer background_image_container;
+
+void handle_init(AppContextRef ctx) {
+  (void) ctx;
+
+  window_init(&window, "Totoro");
+  window_stack_push(&window, true);
+
+  resource_init_current_app(&APP_RESOURCES);
+
+  // Set up a layer for the static watch face background
+  bmp_init_container(RESOURCE_ID_IMAGE_BACKGROUND, &background_image_container);
+  layer_add_child(&window.layer, &background_image_container.layer.layer);
+
+
+  // Date
+  // Initialises the date and sets the text, font and background colour
+  text_layer_init(&text_date_layer, window.layer.frame);
+  text_layer_set_text_color(&text_date_layer, GColorWhite);
+  text_layer_set_background_color(&text_date_layer, GColorClear);
+  layer_set_frame(&text_date_layer.layer, GRect(87, 150, 52, 15));
+
+  text_layer_set_font(&text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_GOTHIC_REGULAR_12)));
+  layer_add_child(&window.layer, &text_date_layer.layer);
+
+  // Time
+  // Initialises the time and sets the text, font and background colour
+  text_layer_init(&text_time_layer, window.layer.frame);
+  text_layer_set_text_color(&text_time_layer, GColorWhite);
+  text_layer_set_background_color(&text_time_layer, GColorClear);
+  layer_set_frame(&text_time_layer.layer, GRect(0, 0, 144, 44));
+
+  text_layer_set_font(&text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_GOTHIC_REGULAR_36)));
+  text_layer_set_text_alignment(&text_time_layer, GTextAlignmentCenter);
+  layer_add_child(&window.layer, &text_time_layer.layer);
+
+}
+
+void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
+
+  (void)ctx;
+
+  // Need to be static because they're used by the system later.
+  static char time_text[] = "00:00";
+  static char date_text[] = "Xxxxxxxxx 00";
+
+  char *time_format;
+
+
+  // TODO: Only update the date when it's changed.
+  string_format_time(date_text, sizeof(date_text), "%B %e", t->tick_time);
+  text_layer_set_text(&text_date_layer, date_text);
+
+  // Caters for  the time format. 
+  if (clock_is_24h_style()) {
+    time_format = "%R";
+  } else {
+    time_format = "%I:%M";
+  }
+
+  string_format_time(time_text, sizeof(time_text), time_format, t->tick_time);
+
+  // Kludge to handle lack of non-padded hour format string
+  // for twelve hour clock.
+  if (!clock_is_24h_style() && (time_text[0] == '0')) {
+    memmove(time_text, &time_text[1], sizeof(time_text) - 1);
+  }
+
+  text_layer_set_text(&text_time_layer, time_text);
+
+}
+
+
+void handle_deinit(AppContextRef ctx) {
+  (void)ctx;
+
+  bmp_deinit_container(&background_image_container);
+}
+
+void pbl_main(void *params) {
+  PebbleAppHandlers handlers = {
+    .init_handler = &handle_init,
+    .deinit_handler = &handle_deinit,
+
+    .tick_info = {
+      .tick_handler = &handle_minute_tick,
+      .tick_units = MINUTE_UNIT
+    }
+
+  };
+  app_event_loop(params, &handlers);
+}
